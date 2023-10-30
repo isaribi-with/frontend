11c87d9c5bb39243a9fdee2b2ad6daf22aabdc8a
codepro2018mba01 <codepro2018mba01@codepro2018mba01noMacBook-Air.local>
2023-10-30 22:14:03 +0900

    logo

--- src/isaribi-home/Home.vue
+++ src/isaribi-home/Home.vue
@@ -1,25 +1,31 @@
 <template>
   <div class="home">
-    <div class="logo">
-      <img src="../assets/example.jpg" alt="">
+    <div class="logo-container">
+      <img src="../assets/logo.jpg" alt="" class="logo" @click="showProjects()">
     </div>
-    <Projects></Projects>
+    <div class="projects" v-if="projects">
+      <router-link to="/event" class="event">
+      </router-link>
+      <router-link to="/product" class="product">
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-import Projects from './Projects.vue';
-
 export default {
   name: 'MyHome',
   components:{
-    Projects
   },
   data() {
     return {
+      projects: false,
     };
   },
   methods: {
+    showProjects(){
+      this.projects = true
+    },
   },
 };
 </script>
@@ -29,12 +35,38 @@ export default {
   width: 100%;
   height: 100%;
 }
+.logo-container{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 .logo{
   width: 50%;
   height: auto;
+  z-index: 3;
 }
-.logo img{
+
+.projects {
   width: 100%;
   height: 100%;
+  position: absolute;
+  top: 0;
+}
+.event,.product {
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  text-align: center;
+  position: absolute;
+}
+.event {
+  left: 20%;
+  background-color: chartreuse;
+}
+.product {
+  left: 60%;
+  background-color: aquamarine;
 }
 </style>
